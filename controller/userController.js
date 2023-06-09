29534696db2e8d7f248f1e557ebfa9f07bef933e
nikhilpounikar <nikhilpounikar@gmail.com>
2023-06-09 17:47:01 +0530

    Partial Used

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,9 @@
 module.exports.profile = function(req,res){
 
-    return res.send('<h1>You are on profile</h1>');
+    // return res.send('<h1>You are on profile</h1>');
+
+    return res.render('profile',{
+
+        person:"Taliq"
+    })
 }
