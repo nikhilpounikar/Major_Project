a38748be901bc8fb82e16af29ef7904b27308f4b
nikhilpounikar <nikhilpounikar@gmail.com>
2023-06-09 14:22:56 +0530

    Index.js configured

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port,function(err){
+
+    if(err){
+        console.log('Error while starting an application : ',err);
+        return;
+    }
+
+    console.log('Server Started on port : ',port);
+})
