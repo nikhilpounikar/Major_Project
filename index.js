a37541c399ef46332669dcd16de0117fb23c25e6
nikhilpounikar <nikhilpounikar@gmail.com>
2023-06-09 18:38:17 +0530

    Database connection done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const port = 8000;
 const layouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 app.use(express.static('./assets'));
 app.set('layout extractStyle ',true);
