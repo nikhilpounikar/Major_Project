3e6ce8f761944f44dcadcaa4d59840896efdd2f5
nikhilpounikar <nikhilpounikar@gmail.com>
2023-06-09 16:17:34 +0530

    Controller setup

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+const homeController = require('../controller/home');
+
 const router = express.Router();
 
 console.log("Router loader");
+
+router.get('/',homeController.home);
 
 module.exports = router;
