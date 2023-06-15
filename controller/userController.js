8cc992bd48b6dd667fb1f575a97d318911c54910
nikhilpounikar <nikhilpounikar@gmail.com>
2023-06-15 09:53:05 +0530

    issue while adding user fix

--- controller/userController.js
+++ controller/userController.js
@@ -23,40 +23,42 @@ module.exports.signUp = function(req,res){
 
 module.exports.signIn = function(req,res){
 
-    // return res.send('<h1>You are on profile</h1>');
 
     return res.render('user_sign_in',{
 
-       // person:"Taliq"
        title:"Sign In"
     })
 }
 
 // creates the users
-module.exports.create = function(req,res){
-
-    if(req.body.password != req.body.confirm_password){
-        return res.redirect('back');
+module.exports.create = function(req, res) {
+    if (req.body.password !== req.body.confirm_password) {
+      return res.redirect('back');
     }
-
-    User.findOne({"email":req.body.email}).then((user)=>{
-
-        if(user == undefined || user == null){
-            User.create(req.body).then((user) =>{
-                console.log('Object saved successfully:', user);
-                return res.redirect('/user/sign-in');
-            }).catch((err) =>{
-                console.error(err);
+  
+    User.findOne({ "email": req.body.email })
+      .then((user) => {
+        if (!user) {
+          User.create(req.body)
+            .then((user) => {
+              console.log('Object saved successfully:');
+              return res.redirect('/user/sign-in');
             })
-        }else{
-            console.log("User Already Exits");
+            .catch((err) => {
+              console.error(err);
+              return res.redirect('back');
+            });
+        } else {
+          console.log("User Already Exists");
+          return res.redirect('back');
         }
-    }).catch((err)=>{
+      })
+      .catch((err) => {
         console.log("Error Finding User", err);
-    })
-   
-    return res.redirect('back');
-}
+        return res.redirect('back');
+      });
+  };
+  
 
 module.exports.createSession = function(req,res){
 
