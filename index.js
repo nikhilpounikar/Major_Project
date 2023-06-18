302a240d93114972dedac75b4d93c26c9f2626b9
nikhilpounikar <nikhilpounikar@gmail.com>
2023-06-18 14:18:23 +0530

    scss configured

--- index.js
+++ index.js
@@ -15,40 +15,53 @@ const passportLocal = require('./config/passport_local_strategy');
 //get mongostore config
 const MongoStore = require('connect-mongo')(session);
 
+// get sass middleware
+const sassMiddleware = require('node-sass-middleware');
+
+// this sassMiddleware should be used before server startup so that it could compiled int0 css prior loading views
+app.use(sassMiddleware({
+
+    src: '/assests/sass',
+    dest:'/assests/css',
+    debug:true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}))
+
 app.use(cookieParser());
 app.use(express.urlencoded());
 app.use(express.static('./assets'));
 
 //puts styles and script to respective postion i.e in head and end the body respectively
-app.set('layout extractStyle ',true);
-app.set('layout extractScripts ',true);
+app.set('layout extractStyle ', true);
+app.set('layout extractScripts ', true);
 // Layouts Should be rendered before routing
 app.use(layouts);
 
 //set up the view engine
-app.set('view engine','ejs');
-app.set('views','./views');
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 //mongo store is being used to store session cookie in db
 app.use(session({
 
-    name:'Major Project',
+    name: 'Major Project',
     // TODO change the secret before deployment in production mode
-    secret:'nikhilpounikar',
+    secret: 'nikhilpounikar',
     saveUninitialized: false,
-    resave:false,
-    cookie:{
-        maxAge :(1000*60*100)
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
     },
 
     store: new MongoStore({
-        mongooseConnection:db,
+        mongooseConnection: db,
         autoRemove: 'disable'
 
     },
-    function(err){
-        console.log(err || 'connect-mongodb setup ok');
-    }
+        function (err) {
+            console.log(err || 'connect-mongodb setup ok');
+        }
     )
 }));
 
@@ -56,15 +69,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 // let middleware handle initial routing
-app.use('/',require('./routes/index'));
+app.use('/', require('./routes/index'));
 
 
-app.listen(port,function(err){
+app.listen(port, function (err) {
 
-    if(err){
-        console.log('Error while starting an application : ',err);
+    if (err) {
+        console.log('Error while starting an application : ', err);
         return;
     }
 
-    console.log('Server Started on port : ',port);
+    console.log('Server Started on port : ', port);
 })
