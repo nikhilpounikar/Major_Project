eb5307ec92febdc0ce1ea3a37365734ad7cdfd7d
nikhilpounikar <nikhilpounikar@gmail.com>
2023-06-19 10:21:53 +0530

    Passport configuration issue fixes

--- config/passport_local_strategy.js
+++ config/passport_local_strategy.js
@@ -17,9 +17,9 @@ passport.use(
             console.log("Invalid Username/Password");
             return done(null, false);
           }
-          if (!user.verifyPassword(password)) {
-            return done(null, false);
-          }
+          // if (!user.verifyPassword(password)) {
+          //   return done(null, false);
+          // }
           return done(null, user);
         })
         .catch((err) => {
@@ -38,41 +38,36 @@ passport.serializeUser(function (user, done) {
 
 // deserialising the user key in cookie
 passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    if (err) {
+  User.findById({ _id: id })
+    .then((user) => {
+      return done(null, user);
+    })
+    .catch((err) => {
       console.log("Error in finding the user ===> Passport");
       return done(err);
-    }
-
-    return done(null, user);
-  });
+    });
 });
 
-
 // check if the user is authenticated
-passport.checkAuthentication = function(req,res,next){
-
-   // if the user aunthenticated allow to navigate anywhere
-   if(req.isAuthenticated()){
+passport.checkAuthentication = function (req, res, next) {
+  // if the user aunthenticated allow to navigate anywhere
+  if (req.isAuthenticated()) {
     return next();
-   }
+  }
 
+  // if user not authenticated navigate the user to sign-in
+  return res.redirect("/user/sign-in");
+};
 
-   // if user not authenticated navigate the user to sign-in
-   return res.redirect('/user/sign-in');
-
-}
-
-passport.setAuthenticatedUser = function(req,res,next){
-
+passport.setAuthenticatedUser = function (req, res, next) {
   // if the user aunthenticated set user info for view
-  if(req.isAuthenticated()){
+  if (req.isAuthenticated()) {
     //req.user contains current signed-in user
     // setting this information for view
     res.locals.user = req.user;
   }
 
   next();
-}
+};
 
 module.exports = passport;
--- controller/userController.js
+++ controller/userController.js
@@ -2,39 +2,46 @@ const User = require("../models/User");
 
 module.exports.profile = function (req, res) {
   // return res.send('<h1>You are on profile</h1>');
-  try {
-    console.log("Cookies", req.cookies);
-    if (req.cookies.user_id) {
-      //find the user
-      User.findOne({ _id: req.cookies.user_id })
-        .then((user) => {
-          // console.log(user);
-          // if user found
-          if (user != null && user != undefined) {
-            //if password doesn't match
-            //console.log(user,req.body);
-            return res.render("profile", {
-              title: "Profile",
-              user: user,
-            });
-          } else {
-            // handle user not found
-            console.log("User Not Found");
-            return res.redirect("/user/sign-in");
-          }
-        })
-        .catch((err) => {
-          // handle error finding user
-          console.log("Error Finding User", err);
-          return res.redirect("/user/sign-in");
-        });
-    } else {
-      return res.redirect("/user/sign-in");
-    }
-  } catch (err) {
-    console.log(err);
-    return res.redirect("/user/sign-in");
-  }
+  /* Manual Authnetication */
+  // try {
+  //   console.log("Cookies", req.cookies);
+  //   if (req.cookies.user_id) {
+  //     //find the user
+  //     User.findOne({ _id: req.cookies.user_id })
+  //       .then((user) => {
+  //         // console.log(user);
+  //         // if user found
+  //         if (user != null && user != undefined) {
+  //           //if password doesn't match
+  //           //console.log(user,req.body);
+  //           return res.render("profile", {
+  //             title: "Profile",
+  //            user: user,
+  //           });
+  //         } else {
+  //           // handle user not found
+  //           console.log("User Not Found");
+  //           return res.redirect("/user/sign-in");
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         // handle error finding user
+  //         console.log("Error Finding User", err);
+  //         return res.redirect("/user/sign-in");
+  //       });
+  //   } else {
+  //     return res.redirect("/user/sign-in");
+  //   }
+  // } catch (err) {
+  //   console.log(err);
+  //   return res.redirect("/user/sign-in");
+  // }
+
+  /* Passport authentication */
+  return res.render("profile", {
+    title: "Profile",
+    //user: user,
+  });
 };
 
 module.exports.signUp = function (req, res) {
@@ -42,8 +49,8 @@ module.exports.signUp = function (req, res) {
 
   //if user is already sign-in redirect to profile
   if (req.isAuthenticated()) {
-    return res.redirect('/user/profile');
-  };
+    return res.redirect("/user/profile");
+  }
 
   return res.render("user_sign_up", {
     // person:"Taliq"
@@ -52,11 +59,10 @@ module.exports.signUp = function (req, res) {
 };
 
 module.exports.signIn = function (req, res) {
-
   //if user is already sign-in redirect to profile
   if (req.isAuthenticated()) {
-    return res.redirect('/user/profile');
-  };
+    return res.redirect("/user/profile");
+  }
 
   return res.render("user_sign_in", {
     // person:"Taliq"
@@ -121,16 +127,14 @@ module.exports.createSession = function (req, res) {
        return res.redirect("back");
      }); */
 
-
   // passport authentication
 
-  return res.redirect('/');
+  return res.redirect("/");
 };
 
 // responsible for sign-out
-module.exports.destroySession = function(req,res){
-
+module.exports.destroySession = function (req, res) {
   req.logout();
 
-  return res.redirect('/');
-}
+  return res.redirect("/");
+};
