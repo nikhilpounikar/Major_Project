d0b548c596e123ddd660778df5596cf4c589f3be
nikhilpounikar <nikhilpounikar@gmail.com>
2023-07-06 17:30:08 +0530

    Google sign-in issue fixes

--- config/passport-google-oauth2-stategy.js
+++ config/passport-google-oauth2-stategy.js
@@ -3,20 +3,22 @@ const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 const crypto = require('crypto');
 
-const User = require('../models/user');
+const User = require('../models/User');
 
 
 // tell passport to use a new strategy for google login
 passport.use(new googleStrategy({
         clientID: '977932929073-fpud2qtp5eb1o7onjs78qomj00sk4rag.apps.googleusercontent.com', // e.g. asdfghjkkadhajsghjk.apps.googleusercontent.com
         clientSecret: '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', // e.g. _ASDFA%KFJWIASDFASD#FAD-
-        callbackURL: "http://localhost:8000/users/auth/google/callback",
+        callbackURL: "http://localhost:8000/user/auth/google/callback",
     },
 
-    function(accessToken, refreshToken, profile, done){
-        // find a user
-        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
-            if (err){console.log('error in google strategy-passport', err); return;}
+    async function(accessToken, refreshToken, profile, done){
+
+        try {
+
+            let user = await User.findOne({email: profile.emails[0].value});
+
             console.log(accessToken, refreshToken);
             console.log(profile);
 
@@ -25,18 +27,26 @@ passport.use(new googleStrategy({
                 return done(null, user);
             }else{
                 // if not found, create the user and set it as req.user
-                User.create({
+                let user = await User.create({
                     name: profile.displayName,
                     email: profile.emails[0].value,
                     password: crypto.randomBytes(20).toString('hex')
-                }, function(err, user){
-                    if (err){console.log('error in creating user google strategy-passport', err); return;}
-
-                    return done(null, user);
                 });
+
+                if(user){
+                    return done(null, user);
+                }else{
+                    console.log('error in creating user google strategy-passport'); return;
+                }
+            
             }
 
-        }); 
+        } catch (error) {
+            console.log('error in google strategy-passport', err);
+            return;
+        }
+        // find a user
+       
     }
 
 
