2d6f39493f8896a3fa77847e6dc616c38a403ef9
nikhilpounikar <nikhilpounikar@gmail.com>
2023-07-11 18:47:04 +0530

    Socket connection is established

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,4 +1,21 @@
 
 module.exports.chatSockets = function(socketServer){
+    const io = require("socket.io")(socketServer, {
+        cors: {
+          origin: "http://localhost:5000",
+          methods: ["GET", "POST"],
+          allowedHeaders: ["my-custom-header"],
+          credentials: true
+        }
+    });
+
+    io.on('connection', function(socket){
+        console.log('new connection received', socket.id);
+
+        socket.on('disconnect', function(){
+            console.log('socket disconnected!');
+        });
+
+    });
 
 }
