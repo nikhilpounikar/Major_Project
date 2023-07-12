5d4385551c92c1d97319f12664a633c2e430df2c
nikhilpounikar <nikhilpounikar@gmail.com>
2023-07-12 17:49:42 +0530

    Chatting is done

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -16,6 +16,20 @@ module.exports.chatSockets = function(socketServer){
             console.log('socket disconnected!');
         });
 
+        socket.on('join_room', function(data){
+            console.log('joining request rec.', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined', data);
+        });
+
+
+         // CHANGE :: detect send_message and broadcast to everyone in the room
+         socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
+        });
+
     });
 
 }
