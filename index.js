610f072efb8b5669bd89e352a7384a54a704d6fa
nikhilpounikar <nikhilpounikar@gmail.com>
2023-07-12 18:06:31 +0530

    Index.js environment configured

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const port = 8000;
 const layouts = require('express-ejs-layouts');
+const env = require('./config/environment');
+
 const db = require('./config/mongoose');
 
 //used for session cookie
@@ -28,12 +30,11 @@ const chatServer = require('http').Server(app);
 const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
-
 // this sassMiddleware should be used before server startup so that it could compiled int0 css prior loading views
 app.use(sassMiddleware({
 
-    src: './assets/scss',
-    dest:'./assets/css',
+    src: path.join(__dirname,env.asset_path,'scss'),
+    dest:path.join(__dirname,env.asset_path,'css'),
     debug:true,
     outputStyle: 'extended',
     prefix: '/css'
@@ -41,7 +42,7 @@ app.use(sassMiddleware({
 
 app.use(cookieParser());
 app.use(express.urlencoded());
-app.use(express.static('./assets'));
+app.use(express.static(env.asset_path));
 
 
 
@@ -63,7 +64,7 @@ app.use(session({
 
     name: 'Major Project',
     // TODO change the secret before deployment in production mode
-    secret: 'nikhilpounikar',
+    secret: env.session_cookie_key,
     saveUninitialized: false,
     resave: false,
     cookie: {
