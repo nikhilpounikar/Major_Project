bd59dd4f003dac46efd45326e950c8370f857aa0
nikhilpounikar <nikhilpounikar@gmail.com>
2023-07-13 18:37:33 +0530

    Gulp rest of the issue to be addressed

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cssnano = require('gulp-cssnano');
 //const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+// const imagemin = require('gulp-imagemin');
+//const del = require('del');
 
-gulp.task('css', function() {
+gulp.task('css', function(done) {
   console.log('minifying css...');
   
   // Process Sass files
-  return gulp.src('./assets/scss/**/*.scss')
+   gulp.src('./assets/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(cssnano())
     .pipe(gulp.dest('./assets/css'))
@@ -17,5 +20,51 @@ gulp.task('css', function() {
     //   base: 'public',
     //   merge: true
     // }))
-    .pipe(gulp.dest('./public/assets'));
+    //.pipe(gulp.dest('./public/assets'));
+
+    done();
+});
+
+
+
+
+gulp.task('js', function(done){
+    console.log('minifying js...');
+     gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    //.pipe(rev())
+    .pipe(gulp.dest('./public/assets/js'))
+    // .pipe(rev.manifest({
+    //     cwd: 'public',
+    //     merge: true
+    // }))
+    //.pipe(gulp.dest('./public/assets'));
+    done()
+});
+
+
+gulp.task('images', function(done){
+    console.log('compressing images...');
+   // gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+    //.pipe(imagemin())
+   // .pipe(rev())
+    //.pipe(gulp.dest('./public/assets'))
+    // .pipe(rev.manifest({
+    //     cwd: 'public',
+    //     merge: true
+    // }))
+    // .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+
+// empty the public/assets directory
+gulp.task('clean:assets', function(done){
+    //del.sync('./public/assets');
+    done();
+});
+
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
+    console.log('Building assets');
+    done();
 });
